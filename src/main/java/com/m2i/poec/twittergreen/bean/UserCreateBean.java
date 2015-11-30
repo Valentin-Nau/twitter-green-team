542d52111eebfcce82ac25c20190d5c15a94a7fd
asma <hamidani.asma@gmail.com>
2015-11-30 12:10:54 +0100

    service

--- src/main/java/com/m2i/poec/twittergreen/bean/UserCreateBean.java
+++ src/main/java/com/m2i/poec/twittergreen/bean/UserCreateBean.java
@@ -1,17 +1,19 @@
 package com.m2i.poec.twittergreen.bean;
 
-import java.util.Date;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.ejb.EJBException;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.flow.builder.ReturnBuilder;
+
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.Column;
 import javax.persistence.PersistenceException;
+import javax.transaction.Status;
 
+import com.m2i.poec.twittergreen.entity.User;
+import com.m2i.poec.twittergreen.service.DuplicateSpecialEmailException;
+import com.m2i.poec.twittergreen.service.DuplicateUsernameException;
 import com.m2i.poec.twittergreen.service.TweeterService;
 
 @RequestScoped
@@ -75,16 +77,67 @@ public class UserCreateBean {
 		return LOGGER;
 	}
 
-	public String createUser() {
+	public String createUser(HashMap<String, String> errors) {
 		try {
+			
 			tweetService.createUser(username, password, email, picture);
-		} catch (EJBException ex) {
+			return "/Tweet.xhtml"; //profil à remettre
 			
+		} catch (Exception e) {
 			
-			return"";
-			
-		}
+			e.printStackTrace();
+			return "/user.xhtml";
 
-		return "";
+//			   if (e.getCause() instanceof PersistenceException) {
+//	                if (e.getCause().getCause() instanceof ConstraintViolationException) {
+	                    
+//	                    ConstraintViolationException ex = (ConstraintViolationException) e.getCause().getCause();
+			
+//						LOGGER.log(Level.INFO, e.toString());
+//			            LOGGER.log(Level.INFO, "constraint name: " + ex.getConstraintName());
+//			            LOGGER.log(Level.INFO, "message :" + ex.getMessage());
+//			            LOGGER.log(Level.INFO, "sql : " + ex.getSQL());
+//			            LOGGER.log(Level.INFO, "error code : " + ex.getErrorCode());
+//			            LOGGER.log(Level.INFO, "Sql state : " + ex.getSQLState());
+//			            
+//			            String msg = "Duplicate entry: "  + ex.getConstraintName();
+//			            if (ex.getErrorCode() == 1062) { // duplicate entry (title)
+//			                throw new DuplicateUsernameException(msg, Status.CONFLICT);
+//			            } else if (ex.getErrorCode() == 1048) { // null constraint (content) 
+//			                throw new WebApplicationException(msg, Status.BAD_REQUEST);
+//			            } else if (ex.getErrorCode() == 1452) { // referential integrity (author)
+//			              throw    new WebApplicationException(msg, Status.BAD_REQUEST);
+//			            }
+					}
+	                
+//			   }
+			   
+//		catch (DuplicateUsernameException e) {
+//
+//			errors.put("username", "This username exists");
+//
+//		} catch (DuplicateSpecialEmailException e) {
+//
+//			errors.put("email", "This special email exists");
+//
+//		} catch (Exception e) {
+//
+//			errors.put("other", "unknown errors");
+//		}
+
+		// on rentre dans la vue avec les erreurs insérées dans le hashmap
+		// qu'on a passé.
+		
 	}
+
+//	public static boolean checkUsernameExists(String username) {
+//		
+//		return false;
+//	}
+//
+//	public static boolean checkSpecialEmailExists(String email) {
+//		
+//		return false;
+//	}
+
 }
--- src/main/java/com/m2i/poec/twittergreen/service/TweeterService.java
+++ src/main/java/com/m2i/poec/twittergreen/service/TweeterService.java
@@ -6,17 +6,15 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import com.m2i.poec.twittergreen.bean.UserCreateBean;
 import com.m2i.poec.twittergreen.entity.Tweet;
 import com.m2i.poec.twittergreen.entity.User;
 
 @Stateless
 public class TweeterService {
 
-<<<<<<< HEAD
 	@PersistenceContext(unitName = "TwitterGreenPU")
-=======
-	@PersistenceContext(unitName="TwitterGreenPU")
->>>>>>> 3046c08c030d624343808b10af50aafd401fc09e
+
 	private EntityManager em;
 
 	public void createTweet(Long author_id, String content, Date creation_Date) {
@@ -25,11 +23,7 @@ public class TweeterService {
 		tweet.setContent(content);
 		tweet.setDate_creation(creation_Date);
 
-<<<<<<< HEAD
 		User user = em.find(User.class, author_id);
-=======
-		User user = em.find(author_id, User.class);
->>>>>>> 3046c08c030d624343808b10af50aafd401fc09e
 
 		tweet.setAuthor(user);
 
@@ -37,14 +31,14 @@ public class TweeterService {
 	}
 
 	public void createUser(String username, String password, String email, String picture) {
-
+		
+		
 		User user = new User();
 		user.setEmail(email);
 		user.setPassword(password);
 		user.setPicture(picture);
 		user.setUsername(username);
-
 		em.persist(user);
-
 	}
+
 }
